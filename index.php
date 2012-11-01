3f2c406853623932aec332c711485957d9ea9043
mahlad <mahlad365@gmail.com>
2012-11-01 20:09:49 +0330

    correct mistake and set the bg

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>UnderDev</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link rel="stylesheet" type="text/css" href="css/1style.css">
+	<link rel="stylesheet" href="css/1styles.css"/>
+	<link rel="stylesheet" href="css/style.css">
 	<script type="text/javascript" src="script/html5shiv.js"></script>
 	<script type="text/javascript" src="script/zepto.js"></script>
 
 </head>
 <body>
+	<div class="out"></div>
 	<?php 
 	include 'mod-contact.php';
 	 ?>
