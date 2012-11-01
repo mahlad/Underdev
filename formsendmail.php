7edbea45aac634a0696261ec41221d012a81470e
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-01 20:54:37 +0330

    ADD FORM.....

--- formsendmail.php
+++ formsendmail.php
@@ -1,5 +1,5 @@
 <div class="designform">
-	<form action="sendmail.php">
-		
+	<form action="sendmail.php" id="form-design" class="form-design" method="post" ntarget="sendinfo">
+		<input type="text" name="name" id="name" placeholder="نام" />
 	</form>
 </div>
