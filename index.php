2e5c2d0552ab164140a9d2578f7269d846c705f2
mahlad <mahlad365@gmail.com>
2012-11-01 19:34:55 +0330

    set the address and new files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>UnderDev</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+
+</head>
+<body>
+	<?php 
+	include 'mod-contact.php';
+	 ?>
+</body>
+</html>
--- mod-contact.php
+++
@@ -1,1 +0,0 @@
-mod-contact.php
