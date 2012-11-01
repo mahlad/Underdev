3e85a318c1264e92e7e0daddf73a0232de0f07eb
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-01 20:59:32 +0330

    add..

--- formsendmail.php
+++ formsendmail.php
@@ -1,5 +1,7 @@
-<div class="designform">
+<section class="designform">
 	<form action="sendmail.php" id="form-design" class="form-design" method="post" ntarget="sendinfo">
 		<input type="text" name="name" id="name" placeholder="نام" />
+		<input type="text" name="subject" id="subject" placeholder="موضوع" />
+		<textarea cols="5" rows="10" id="text" name="text"></textarea>
 	</form>
-</div>
+</section>
