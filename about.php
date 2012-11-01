c293ee3f4154a01431901a4797a2a079ebac0bfe
mahlad <mahlad365@gmail.com>
2012-11-02 02:06:23 +0330

    compelet about

--- about.php
+++ about.php
@@ -1,3 +1,42 @@
 <div class="about">
-	
+	<table>
+		<tr>
+			<th><p class="left">mahla delpak</p></th>
+			<th></th>
+			<th><p class="right">zohre zahedy</p></th>
+		</tr>
+		<tr>
+			<td>
+				<a class="left" href="https://github.com/mahlad" title="My github">https://github.com/mahlad</a>
+			</td>
+			<td class="icon">
+				<img src="images/github.png" alt="My github">
+			</td>
+			<td>
+				<a class="right" href="https://github.com/zohreh-z" title="My github">https://github.com/zohreh-z</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="left" href="http://mahlad.ir" title="My website">http://mahlad.ir/</a>
+			</td>
+			<td class="icon">
+				<img class="email" src="images/www.png" alt="My website">
+			</td>
+			<td >
+				<a class="right" href="http://www.zahedy.ir" title="My website">http://www.zahedy.ir</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p class="left">mahlad365@gmail.com</p>
+			</td>
+			<td class="icon">
+				<img class="email"src="images/gmail.png" alt="My github">
+			</td>
+			<td>
+				<p class="right">zahedi.zohreh@gmail.com</p>
+			</td>
+		</tr>
+	</table>
 </div>
