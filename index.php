7e4cb2938423a398db3b7a86dabb127db112fa52
mahlad <mahlad365@gmail.com>
2012-11-01 19:47:54 +0330

    elementary needs

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>UnderDev</title>
-	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/1style.css">
+	<script type="text/javascript" src="script/html5shiv.js"></script>
+	<script type="text/javascript" src="script/zepto.js"></script>
 
 </head>
 <body>
