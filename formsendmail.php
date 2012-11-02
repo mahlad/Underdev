8b7f0a4a635989bfbbfa692438c2af2b0278efb5
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-02 09:10:39 +0330

    add php send mail...

--- formsendmail.php
+++ formsendmail.php
@@ -3,7 +3,7 @@
 	<form action="sendmail.php" id="form-design" class="form-design" method="post" ntarget="sendinfo">
 		<input type="text" name="name" id="name" placeholder="نام" />
 		<input type="text" name="subject" id="subject" placeholder="موضوع" />
-		<textarea cols="8" rows="4" id="text" name="text" placeholder="متن"></textarea>
+		<textarea cols="8" rows="4" id="txt" name="txt" placeholder="متن"></textarea>
 		<input type="submit" name="submit" value="ارسال پیام" />
 	</form>
 	<a href="#" class="contact btn">Contact</a>
